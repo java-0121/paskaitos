4a80764c705ff964eeb2f9e858792c660ef55cdd
Paulius Zubavičius <paulius@zubavicius.lt>
2019-01-25 14:16:09 +0200

    bytes task

---
+++ 05_if_switch/src/main/java/com/vcs/lects/l05/ifswitches/tasks/ByteConverter.java
@@ -0,0 +1,23 @@
+package com.vcs.lects.l05.ifswitches.tasks;
+
+public class ByteConverter {
+
+	
+	/**
+	 * 0 bytes ...; 1kb ... ; 1Mb... 1Gb ... ; 1Tb and more;
+	 *
+	 * Example: 1,3Gb // <- Acceptable 893Kb // <- Acceptable 1Gb and 312Mb and
+	 * ...// <- Not acceptable
+	 *
+	 *
+	 *
+	 * http://byteconvert.org/
+	 *
+	 */
+	
+	public Object convertToHumanReadable(long tb) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
