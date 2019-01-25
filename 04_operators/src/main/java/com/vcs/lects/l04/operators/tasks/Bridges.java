808a34bc90a16f6301afadadde6fbf52cccca2ae
Paulius Zubavičius <paulius@zubavicius.lt>
2019-01-25 14:08:41 +0200

    bridges solution

--- 04_operators/src/main/java/com/vcs/lects/l04/operators/tasks/Bridges.java
+++ 04_operators/src/main/java/com/vcs/lects/l04/operators/tasks/Bridges.java
@@ -7,8 +7,18 @@ public class Bridges {
 	 */
 	public boolean getWay(boolean p1, boolean p2, boolean p3, boolean p4, boolean p5, boolean p6) {
 
-		//TODO
-		return false;
+		boolean w1 = p1 && p2;
+		boolean w2 = p4 && p5;
+		boolean w3 = p4 && p3 && p2;
+		boolean w4 = p1 && p3 && p5;
+		boolean w5 = p4 && p6 && p2;
+
+		return w1 || w2 || w3 || w4 || w5;
+	}
+
+	public boolean getWay2(boolean p1, boolean p2, boolean p3, boolean p4, boolean p5, boolean p6) {
+
+		return ! (( !p1 && !p4 ) || (!p2 && !p5));
 	}
 
 }
