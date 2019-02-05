1669db7d0c488a1a92ec35d6359f1f189acf1be6
Paulius Zubavičius <paulius@zubavicius.lt>
2019-02-05 15:49:59 +0200

    avg calc solution

--- 06_arrays_cycles/src/main/java/com/vcs/lects/l06/arrays/task/average/AverageCalculator.java
+++ 06_arrays_cycles/src/main/java/com/vcs/lects/l06/arrays/task/average/AverageCalculator.java
@@ -2,13 +2,28 @@ package com.vcs.lects.l06.arrays.task.average;
 
 public class AverageCalculator {
 
+	public static void main(String... strings) {
+
+		AverageCalculator avg = new AverageCalculator();
+
+		double[] data = { Double.MAX_VALUE, Double.MAX_VALUE };
+
+		System.out.println(avg.calcAverage(data));
+
+	}
+
 	/**
 	 * Vidurkio skaiciavimas
 	 */
 
 	public double calcAverage(double[] data) {
 
-		return 0;
+		double sum = 0;
+		for (double d : data) {
+			sum += d / data.length;
+		}
+
+		return sum;
 	}
 
 }
