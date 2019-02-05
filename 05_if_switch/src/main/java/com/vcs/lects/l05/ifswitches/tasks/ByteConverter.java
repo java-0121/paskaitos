29d578f3cb5c6c911cbc57c67d9743c6910eb828
Paulius Zubavičius <paulius@zubavicius.lt>
2019-02-05 15:40:47 +0200

    size converter solution

--- 05_if_switch/src/main/java/com/vcs/lects/l05/ifswitches/tasks/ByteConverter.java
+++ 05_if_switch/src/main/java/com/vcs/lects/l05/ifswitches/tasks/ByteConverter.java
@@ -14,10 +14,44 @@ public class ByteConverter {
 	 *
 	 */
 
+	private static final long _1024 = 1024L;
+
+	private static final long B = 1;
+	private static final long KB = _1024;
+	private static final long MB = KB * _1024;
+	private static final long GB = MB * _1024;
+	private static final long TB = GB * _1024;
+
 	public String convertToHumanReadable(long sizeInBytes) {
 
-		// TODO
-		return "";
+		if (sizeInBytes < KB) {
+			return "" + sizeInBytes + " b";
+		}
+
+		if (sizeInBytes < MB) {
+			return "" + sizeInBytes / KB + " Kb";
+		}
+
+		if (sizeInBytes < GB) { //
+			return "" + cutDigigts((1.0 * sizeInBytes / MB)) + " Mb";
+		}
+
+		if (sizeInBytes < TB) { // GB
+			return "" + cutDigigts((1.0 * sizeInBytes / GB)) + " Gb";
+		}
+
+		return "" + cutDigigts((1.0 * sizeInBytes / TB)) + " Tb";
+	}
+
+	private String cutDigigts(double d) {
+		d *= 10;
+		long lng = (long) d;
+
+		if (lng % 10 == 0) {
+			return "" + lng / 10;
+		}
+
+		return "" + lng / 10.0;
 	}
 
 }
