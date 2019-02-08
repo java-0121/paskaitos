eef0974357248daea330feccea025d137a6386a5
Paulius Zubavičius <paulius@zubavicius.lt>
2019-02-08 13:57:51 +0200

    numbers ordering solution

--- 06_arrays_cycles/src/main/java/com/vcs/lects/l06/arrays/task/sort/NumbersOrdering.java
+++ 06_arrays_cycles/src/main/java/com/vcs/lects/l06/arrays/task/sort/NumbersOrdering.java
@@ -11,8 +11,57 @@ public class NumbersOrdering {
 	 */
 
 	public int[] sort(String data, boolean ascending) {
-		// TODO ...
-		return null;
+
+		int[] dataInt = parseData(data);
+
+		return sortArray(dataInt, ascending);
+	}
+
+	private int[] sortArray(int[] dataInt, boolean ascending) {
+
+		int indexWithMinVal = 0;
+		for (int i = 0; i < dataInt.length; i++) {
+
+			indexWithMinVal = findIndexWithExremum(i, dataInt, ascending);
+			swapValuesInArr(dataInt, i, indexWithMinVal);
+		}
+
+		return dataInt;
+	}
+
+	private void swapValuesInArr(int[] dataInt, int index1, int index2) {
+
+		int temp = dataInt[index1];
+		dataInt[index1] = dataInt[index2];
+		dataInt[index2] = temp;
+
+	}
+
+	private int findIndexWithExremum(int startSearchIndex, int[] dataInt, boolean ascending) {
+
+		int currentVal = (ascending ? Integer.MAX_VALUE : Integer.MIN_VALUE);
+		int newValIndex = 0;
+		for (int i = startSearchIndex; i < dataInt.length; i++) {
+			if ((ascending && dataInt[i] < currentVal) || (!ascending && dataInt[i] > currentVal)) {
+				currentVal = dataInt[i];
+				newValIndex = i;
+			}
+		}
+
+		return newValIndex;
+	}
+
+	private int[] parseData(String data) {
+
+		String[] strs = data.split(" ");
+
+		int[] result = new int[strs.length];
+
+		for (int i = 0; i < strs.length; i++) {
+			result[i] = Integer.parseInt(strs[i]);
+		}
+
+		return result;
 	}
 
 }
